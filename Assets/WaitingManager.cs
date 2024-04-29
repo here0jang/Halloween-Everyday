8b08318317e6ded0222da7a07ee235e32e486be5
here0jang <here0jang@gmail.com>
2024-04-29 18:05:34 +0900

    코드 정리

--- Assets/WaitingManager.cs
+++ Assets/WaitingManager.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Services.Authentication;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class WaitingManager : MonoBehaviour
 {
